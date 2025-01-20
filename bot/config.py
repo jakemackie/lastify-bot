f5f57110b16053eb762c4e72ea0096e8379b0380
Jake Mackie <jake@hiyield.co.uk>
2025-01-20 14:57:22 +0000

    feat: Greatly improve setup

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,9 @@
 from tomllib import load
+from pathlib import Path
 
-def load_config(path="config.toml"):
-    with open(path, "rb") as f:
+def load_config(path="config/config.toml"):
+    root = Path(__file__).parent.parent
+    config_path = root / path
+    
+    with open(config_path, "rb") as f:
         return load(f) 
--- main.py
+++ main.py
@@ -3,16 +3,22 @@ import logging
 from bot.client import create_bot
 from bot.config import load_config
 from utils.load_module import load_modules
+from config.logging import logging_init
 
 logger = logging.getLogger(__name__)
 
 async def main():
+    logging_init()
+
     config = load_config()
     
     bot = create_bot(config)
     
-    # Load all modules
-    await load_modules(bot, ["jishaku", "cmds", "events"])
+    await load_modules(bot, [
+        "jishaku", 
+        "cmds", 
+        "events"
+    ])
     
     logger.info("Starting bot...")
     await bot.start(config["bot"]["token"])
