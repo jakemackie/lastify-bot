9f4871a9e133d44c4671da77eb7a97a987e4fbb7
Jake Mackie <jake@hiyield.co.uk>
2025-01-20 16:00:27 +0000

    amend: only import what we need

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import asyncio
-import logging
+from logging import getLogger
 from bot.client.setup import create_bot
 from bot.core.config import load_config
 from bot.core.logging import logging_init
 from bot.modules.loader import load_modules
 
-logger = logging.getLogger(__name__)
+logger = getLogger(__name__)
 
 async def main():
     logging_init()
